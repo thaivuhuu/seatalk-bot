c09fc30f3e7607c8eb16fcfc91bc8aec9647ac9d
thaivuhuu <thai.vuhuu@spxexpress.com>
2026-07-11 09:24:34 +0700

    Create server.js

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Seatalk Bot is running.");
+});
+
+const PORT = process.env.PORT || 10000;
+
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
